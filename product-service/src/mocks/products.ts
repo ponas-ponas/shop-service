209e1efe5b5e1f6bb9ca5aeee9751dd4b45488c9
Andrei Shchatko <andrei_shchatko@epam.com>
2022-10-13 09:08:57 +0300

    Add description field to Product

--- product-service/src/mocks/products.ts
+++ product-service/src/mocks/products.ts
@@ -1,18 +1,19 @@
 interface Product {
-  id: number,
+  id: string,
   name: string,
   price: number,
+  description: string;
   imageUrl?: string
 }
 
 export const products: Product[] = [
-  { id: 1, name: 'Great book', price: 200 },
-  { id: 2,  name: 'Good book', price: 500 },
-  { id: 3,  name: 'Great book', price: 100 },
-  { id: 4,  name: 'Good book', price: 250 },
-  { id: 5,  name: 'Great book', price: 300 },
-  { id: 6,  name: 'Good book', price: 75 },
-  { id: 7,  name: 'Good book', price: 125 }
+  { id: '1', name: 'Great book', description: 'Great novel ever', price: 200 },
+  { id: '2',  name: 'Good book', description: 'Something intersting', price: 500 },
+  { id: '3',  name: 'Great book', description: 'Wow such a book', price: 100 },
+  { id: '4',  name: 'Good book', description: 'Amazing characters!!', price: 250 },
+  { id: '5',  name: 'Great book', description: 'Don\'t miss this book, it\'s good', price: 300 },
+  { id: '6',  name: 'Good book', description: 'Like it', price: 75 },
+  { id: '7',  name: 'Good book', description: 'read it', price: 125 }
 ];
 
 export const fetchProducts = () => new Promise<Product[]>(resolve => resolve(products));
